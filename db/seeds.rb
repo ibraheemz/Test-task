d3112820dd5b0833bc4381c31ea3e52ad280b44b
ibraheemz <ibrahemrwash18@gmail.com>
2024-01-12 15:33:02 +0200

    added articles seed

---
+++ db/seeds.rb
@@ -0,0 +1,49 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+articles = Article.create([
+    {
+    title: 'Seed Object 1',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget justo euismod, tincidunt quam a, venenatis leo. Curabitur et elit vel odio sodales sodales.',
+  },
+  {
+    title: 'Seed Object 2',
+    body: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed eu leo vitae metus condimentum varius non vitae elit. Vivamus in urna sapien.',
+  },
+  {
+    title: 'Seed Object 3',
+    body: 'Vivamus vestibulum odio id libero blandit, vel accumsan elit ullamcorper. Aenean et mi bibendum, sagittis neque eu, eleifend lectus. Nam convallis velit vitae risus tincidunt, vel pharetra elit luctus.',
+  },
+  {
+    title: 'Seed Object 4',
+    body: 'Fusce ut justo vel odio posuere pellentesque. Suspendisse potenti. Proin ultricies velit vitae metus lacinia, eu dictum augue consequat. Sed eu arcu non nunc tristique eleifend.',
+  },
+  {
+    title: 'Seed Object 5',
+    body: 'Nullam scelerisque urna at justo ullamcorper, at interdum libero dapibus. Nulla facilisi. Vestibulum vel vestibulum orci. Nunc suscipit arcu nec tellus eleifend, vel cursus dolor feugiat.',
+  },
+  {
+    title: 'Seed Object 6',
+    body: 'Maecenas ut elit non sem fermentum tincidunt. Praesent ac ultricies velit. Curabitur fermentum turpis eget ex scelerisque scelerisque. Sed ut augue ac velit aliquet efficitur ut vitae massa.',
+  },
+  {
+    title: 'Seed Object 7',
+    body: 'Donec fringilla augue nec ipsum varius, vel feugiat libero tincidunt. Aenean eget arcu sed ligula facilisis posuere id et eros. Phasellus vel efficitur felis. Fusce ultrices cursus justo.',
+  },
+  {
+    title: 'Seed Object 8',
+    body: 'Aliquam ultrices efficitur dui, at congue velit aliquet at. Vestibulum sed ante vel dolor vehicula venenatis. Mauris at fermentum nisi. Ut sed felis ac purus hendrerit sodales.',
+  },
+  {
+    title: 'Seed Object 9',
+    body: 'Cras nec arcu sit amet velit egestas interdum. Integer et ante non risus efficitur laoreet. Aenean et tortor eget nulla facilisis accumsan. Fusce vel massa ut quam mattis malesuada.',
+  },
+  {
+    title: 'Seed Object 10',
+    body: 'Quisque id justo ac tortor ultricies commodo vel id purus. Praesent quis erat vel dui mattis volutpat nec nec orci. Mauris vel justo ut augue ullamcorper hendrerit. Suspendisse potenti. Ut euismod feugiat fermentum.',
+  }
+])
